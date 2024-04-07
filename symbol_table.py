aff56a841d8b4fd1751a2f64efb56b859fdaa764
martin-minarik <mn.minarik@gmail.com>
2024-04-07 19:09:17 +0200

    Add SymbolTable

---
+++ symbol_table.py
@@ -0,0 +1,28 @@
+from type_enum import Type
+from errors import Errors
+from typing import Any
+from antlr4.Token import Token
+
+
+class SymbolTable:
+    def __init__(self):
+        self.memory: dict[str, tuple[Type, Any]] = {}
+
+    def add(self, variable: Token, type_: Type) -> None:
+        name = variable.text.strip()
+        if name in self.memory:
+            Errors.report_error(variable, f"Variable {name} was already declared.")
+        else:
+            self.memory[name] = (type_, 0 if type_ == Type.Int else 0.0)
+
+    def __getitem__(self, variable: Token) -> tuple[Type, Any]:
+        name = variable.text.strip()
+        if name in self.memory:
+            return self.memory[name]
+        else:
+            Errors.report_error(variable, f"Variable {name} was NOT declared.")
+            return Type.Error, 0
+
+    def __setitem__(self, variable: Token, value: Any) -> None:
+        name = variable.text.strip()
+        self.memory[name] = value
