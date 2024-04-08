da18bd6a3cdce06ef640f982f6961db6ca55b0ce
martin-minarik <mn.minarik@gmail.com>
2024-04-08 15:01:07 +0200

    Rename param in Errors

--- errors.py
+++ errors.py
@@ -1,6 +1,7 @@
 from antlr4.Token import Token
 from antlr4.ParserRuleContext import ParserRuleContext
 
+
 class Errors:
     errors_data = []
 
@@ -9,8 +10,8 @@ class Errors:
         Errors.errors_data.append(f"{token.line}:{token.column} - {message}")
 
     @staticmethod
-    def report_error_rule_context(token: ParserRuleContext, message):
-        Errors.errors_data.append(f"{token.start.line}:{token.start.column} - {message}")
+    def report_error_rule_context(rule_context: ParserRuleContext, message):
+        Errors.errors_data.append(f"{rule_context.start.line}:{rule_context.start.column} - {message}")
 
     @staticmethod
     def number_of_errors():
